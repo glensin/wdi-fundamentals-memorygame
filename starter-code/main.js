e377487938e3b707c9c68ad79beef53a31109720
Sin Chin Guan <sinchinguanglen@203.203-211-151.static.qala.com.sg>
2016-11-29 18:59:14 +0800

    unit 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,19 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king"
-if (cardOne===cardTwo) {alert("You found a match!");}
+var gameboard = document.getElementById ('game-board');
+function createBoard (){
+for (var i=0; i <cards.length ; i ++);{
+var createnew = document.createElement ('div');
+createnew.classname = ('cards');
+gameboard.appendChild (createnew);} }
+
+
+
+
+/*if (cardOne===cardTwo) {alert("You found a match!");}
 else if (cardThree===cardFour) {window.alert("You found a match!");}
 else if (cardTwo===cardFour){window.alert("sorry, try again");}
 else if (cardOne===cardThree){window.alert("sorry, try again");}
 else if (cardTwo===cardThree){window.alert("sorry, try again");}
-else (cardOne===cardFour){window.alert("sorry, try again")}
+else (cardOne===cardFour){window.alert("sorry, try again")}/
