c52e8cbb338b87bfd94c0486f4b6ec4a5ecfa6b9
Sin Chin Guan <sinchinguanglen@203.203-211-151.static.qala.com.sg>
2016-11-29 11:29:41 +0800

    update file

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,9 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king"
+if (cardOne===cardTwo) {alert("You found a match!");}
+else if (cardThree===cardFour) {window.alert("You found a match!");}
+else if (cardTwo===cardFour){window.alert("sorry, try again");}
+else if (cardOne===cardThree){window.alert("sorry, try again");}
+else if (cardTwo===cardThree){window.alert("sorry, try again");}
+else (cardOne===cardFour){window.alert("sorry, try again")}
